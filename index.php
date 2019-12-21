2d3fd252616192459f09b144462dbefc271fbffe
429272-13 <429272-13>
2019-12-21 11:37:05 +0300

    Lab 2

--- index.php
+++ index.php
@@ -1,20 +1,54 @@
 <?php
-//class B {
 
+//Class Kupris{								//линейное
+//protected $x;
+//protected function _construct($result = 0 ){
+//$this->x = $result;
 //}
-//Class ClassA extends ClassB {
-//	public function _construcct ($obj1, $obj2,$obj3) {
-//	$this->$obj1 = $obj1;
-//$this->$obj2 = $obj2;
-//$this->$obj3 = $obj3;
-//	}
-//	public $obj1;
-//	public $obj2;
-//	public $obj3;
+//protected function getX() {
+//return $this->result;
 //}
-//$obj1 = new ClassB();
-//$obj2 = new ClassB();
-//$obj3 = new ClassB();
-//$obj4 = new ClassA($obj1);
-//$obj5 = new ClassA($obj2, $obj3, $obj4);
-?>
+//protected function setX($val) {
+//return $this->x = $val;
+//}
+//protected function rut($a,$b){
+//$this->x = ($b* -1)/$a;
+//return $this->x;
+//}
+//}
+//Class  Wake extends  Kupris {			//квадратное
+//protected function _construct($obj1, $obj2=null, $obj3=null, $result = 0){
+//$this->obj1 = $obj1;
+//$this->obj2 = $obj2;
+//$this->obj3 = $obj3;
+//}
+//protected function getObj1(){
+//return $this->obj1;
+//}
+//protected function setObj1($val){
+//$this->obj1 = $val;
+//}
+//protected function Disc($a,$b,$c){
+//return pow($b,2) - 4*$a*$c;
+//}  
+//protected function Cvad ($a,$b,$c){
+//$disc = $this->Disc($a,$b,$c);
+//if($disc>0){
+//$this->x = Array();
+//$x1 = (-1*$b+sqrt($disc))/(2*$a);
+//$x2 = (-1*$b-sqrt($disc))/(2*$a);
+//array_push($this->x, $x1, $x2);
+//}
+//if($disc == 0){
+//$this->x = (-1*$b)/(2*$a);
+//}		
+//return $this->x;
+//}
+//protected $obj1;
+//protected $obj2;
+//protected $obj3;
+//}
+ 
+ ?>
+ 
+ 
