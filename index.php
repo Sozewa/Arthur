0a86c37b8ce453eeb6e921391843c0fa90d66ded
429272-13 <429272-13>
2019-12-21 11:44:04 +0300

    Lab 4

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(-1);
+
+ini_set("display_errors", 1);
+error_reporting (-1); 
+
+use Kupris\KuprisException;
+use Kupris\MyLog;
+use Kupris\Linear;
+use Kupris\Square;
+
 
 include 'core/EquationInterface.php';
 include 'core/LogInterface.php';
@@ -10,3 +17,20 @@ include 'Kupris/MyLog.php';
 include 'Kupris/Linear.php';
 include 'Kupris/Square.php';
 
+
+
+	try{
+		echo 'Enter a, b, c' . "\n";
+		$num = [];
+		for($i = 0; $i < 3; $i++){
+			$num[$i] = readline("Value = ");	
+		}
+		MyLog::log("Vvedeno uravneniye: " . $num[0] . "x^2 + " . $num[1] . "x + " . $num[2] . " = 0");
+		$square = new Square();
+		$roots = $square->solve($num[0], $num[1], $num[2]);
+		MyLog::log("Korni uravneniye: " . implode(",", $roots) . "\n");
+	} catch(KuprisException $e) {
+		MyLog::log($e->getMessage());
+	}
+
+MyLog::write();
